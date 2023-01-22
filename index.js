22fb0dafcb9eba0769665a5b52dbc0f102912cf3
tylerthayn <tylerthayn@hotmail.com>
2023-01-21 21:27:27 -0700

    '.'

---
+++ index.js
@@ -0,0 +1,15 @@
+
+
+require('@tyler.thayn/js.core')
+require('fs')
+require('path')
+require('./tasks/bump')
+require('./tasks/readme')
+require('./lib/AST')
+require('./lib/Docs')
+require('./bin/cli')
+require('./templates/docdash/publish')
+require('./templates/docstrap/publish')
+require('./templates/readme')
+
+
